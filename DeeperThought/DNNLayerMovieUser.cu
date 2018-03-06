6002a96d8c4be3097065109d2c978f9b618d9167
Peter Taraba <pepe78@users.noreply.github.com>
2018-03-05 22:37:45 -0800

    adding offset

--- DeeperThought/DNNLayerMovieUser.cu
+++ DeeperThought/DNNLayerMovieUser.cu
@@ -27,12 +27,12 @@ __global__ void movieuser_forward(float *outp, const int *inp, const float *para
 			user[i] = params[numMovies *vectorWidthMovie + u * vectorWidthUser + i];
 		}
 
-		float tmp = 0;
+		float tmp = params[numMovies * vectorWidthMovie + numUsers * vectorWidthUser];
 		for (int i = 0; i < vectorWidthMovie; i++)
 		{
 			for (int j = 0; j < vectorWidthUser; j++)
 			{
-				tmp += movie[i] * movie[i] * user[j] * user[j] * params[numMovies * vectorWidthMovie + numUsers * vectorWidthUser + i * vectorWidthUser + j];
+				tmp += movie[i] * movie[i] * user[j] * user[j] * params[numMovies * vectorWidthMovie + numUsers * vectorWidthUser + i * vectorWidthUser + j + 1];
 			}
 		}
 		outp[tid] = tmp;
@@ -64,14 +64,15 @@ __global__ void movieuser_backward(float *dparams, const float *doutp, const flo
 			duser[i] = 0;
 		}
 
+		atomicAdd(&(dparams[numMovies * vectorWidthMovie + numUsers * vectorWidthUser]), doutp[tid]);
 		for (int i = 0; i < vectorWidthMovie; i++)
 		{
 			for (int j = 0; j < vectorWidthUser; j++)
 			{
-				dmovie[i] += doutp[tid] * movie[i] * 2.0f * user[j] * user[j] * params[numMovies * vectorWidthMovie + numUsers * vectorWidthUser + i * vectorWidthUser + j];
-				duser[j] += doutp[tid] * movie[i] * movie[i] * user[j] * 2.0f * params[numMovies * vectorWidthMovie + numUsers * vectorWidthUser + i * vectorWidthUser + j];
+				dmovie[i] += doutp[tid] * movie[i] * 2.0f * user[j] * user[j] * params[numMovies * vectorWidthMovie + numUsers * vectorWidthUser + i * vectorWidthUser + j + 1];
+				duser[j] += doutp[tid] * movie[i] * movie[i] * user[j] * 2.0f * params[numMovies * vectorWidthMovie + numUsers * vectorWidthUser + i * vectorWidthUser + j + 1];
 
-				atomicAdd(&(dparams[numMovies * vectorWidthMovie + numUsers * vectorWidthUser + i * vectorWidthUser + j]), doutp[tid] * movie[i] * movie[i] * user[j] * user[j]);
+				atomicAdd(&(dparams[numMovies * vectorWidthMovie + numUsers * vectorWidthUser + i * vectorWidthUser + j + 1]), doutp[tid] * movie[i] * movie[i] * user[j] * user[j]);
 			}
 		}
 
@@ -87,7 +88,7 @@ __global__ void movieuser_backward(float *dparams, const float *doutp, const flo
 }
 
 DNNLayerMovieUser::DNNLayerMovieUser(int _numMovies, int _numUsers, int _vectorWidthMovie, int _vectorWidthUser, float _initValues, float _stepSize, int _batchSize)
-	: DNNLayer(_batchSize, 2, 1, _numMovies * _vectorWidthMovie + _numUsers * _vectorWidthUser + _vectorWidthMovie * _vectorWidthUser, _initValues, _stepSize)
+	: DNNLayer(_batchSize, 2, 1, _numMovies * _vectorWidthMovie + _numUsers * _vectorWidthUser + (1 + _vectorWidthMovie * _vectorWidthUser), _initValues, _stepSize)
 {
 	numMovies = _numMovies;
 	numUsers = _numUsers;
