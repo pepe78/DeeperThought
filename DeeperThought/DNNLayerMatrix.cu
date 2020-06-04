915f721a42eeaea25a7577ee9099a9fd6eeeee1e
Peter Taraba <taraba.peter@mail.com>
2020-06-04 12:55:47 -0700

    speeding up matrix layer with use of local memory

--- DeeperThought/DNNLayerMatrix.cu
+++ DeeperThought/DNNLayerMatrix.cu
@@ -1,7 +1,14 @@
 #include "DNNLayerMatrix.cuh"
+#include "GPU.cuh"
 
 #include <cstdlib>
 #include <cstdio>
+#include <chrono>
+
+using namespace std;
+
+#define MAXINP 5000
+#define MAXOUTP 5000
 
 __global__ void matrix_forward(float *outp, const float *inp, const float *pars, int inputWidth, int outputWidth, int batchSize)
 {
@@ -9,12 +16,18 @@ __global__ void matrix_forward(float *outp, const float *inp, const float *pars,
 
 	if (tid < batchSize)
 	{
+		float inp2[MAXINP];
+		for(int i=0;i<inputWidth;i++)
+		{
+			inp2[i] = inp[tid* inputWidth + i];
+		}
+	
 		for (int i = 0; i < outputWidth; i++)
 		{
 			float tmp = pars[i * (inputWidth + 1)];
 			for (int j = 0; j < inputWidth; j++)
 			{
-				tmp += pars[i * (inputWidth + 1) + 1 + j] * inp[tid* inputWidth + j];
+				tmp += pars[i * (inputWidth + 1) + 1 + j] * inp2[j];
 			}
 			outp[tid * outputWidth + i] = tmp;
 		}
@@ -27,16 +40,27 @@ __global__ void matrix_backward(float *dinp, float *dpars, const float *doutp, c
 
 	if (tid < batchSize)
 	{
+		float inp2[MAXINP];
+		for(int i=0;i<inputWidth;i++)
+		{
+			inp2[i] = inp[tid* inputWidth + i];
+		}
+		float doutp2[MAXOUTP];
+		for(int i=0;i<outputWidth;i++)
+		{
+			doutp2[i] = doutp[tid * outputWidth + i];
+		}
+	
 		for (int i = 0; i < outputWidth; i++)
 		{
-			atomicAdd(&(dpars[i * (inputWidth + 1)]), doutp[tid * outputWidth + i]);
+			atomicAdd(&(dpars[i * (inputWidth + 1)]), doutp2[i]);
 			for (int j = 0; j < inputWidth; j++)
 			{
 				if (dinp != NULL)
 				{
-					dinp[tid* inputWidth + j] += doutp[tid * outputWidth + i] * pars[i * (inputWidth + 1) + 1 + j];
+					dinp[tid* inputWidth + j] += doutp2[i] * pars[i * (inputWidth + 1) + 1 + j];
 				}
-				atomicAdd(&(dpars[i * (inputWidth + 1) + 1 + j]), doutp[tid * outputWidth + i] * inp[tid* inputWidth + j]);
+				atomicAdd(&(dpars[i * (inputWidth + 1) + 1 + j]), doutp2[i] * inp2[j]);
 			}
 		}
 	}
@@ -45,6 +69,11 @@ __global__ void matrix_backward(float *dinp, float *dpars, const float *doutp, c
 DNNLayerMatrix::DNNLayerMatrix(int _inputWidth, int _outputWidth, int _batchSize, float _initVal, float _stepSize)
 	: DNNLayer(_batchSize, _inputWidth, _outputWidth, _outputWidth * (_inputWidth + 1), _initVal, _stepSize)
 {
+	if (_inputWidth > MAXINP || _outputWidth > MAXOUTP)
+	{
+		fprintf(stderr, "Project needs to be recompiled with larger field for matrix layer\n");
+		exit(-1);
+	}
 }
 
 DNNLayerMatrix::~DNNLayerMatrix()
